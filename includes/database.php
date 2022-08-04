f1fed4ee20a52f455a9da796d142a54b15df6caa
jsgiovani <josgiovani@hotmail.com>
2022-08-04 15:22:36 -0400

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,11 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', 'uptask');
+$host = $_ENV["DB_HOST"];
+$user = $_ENV["DB_USER"];
+$password = $_ENV["DB_PASSWORD"];
+$db_name = $_ENV["DB_NAME"];
+
+$db = mysqli_connect($host, $user, $password, $db_name);
 
 
 if (!$db) {
